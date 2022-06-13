3eb7833d060b0d07750d0933b1f3ec32ff74d48b
Debby95 <debbyjacksonstar@hotmail.it>
2022-06-13 23:22:56 +0200

    + layout e lista

---
+++ js/script.js
@@ -0,0 +1,31 @@
+//creo una lista 
+new Vue ({
+    el: '#app',
+    data: {
+        toDoList: [
+            {
+                text: "T-shirt",
+                done: true,
+            },
+            {
+                text: "Jeans",
+                done: false,
+            },
+            {
+                text: "Collana",
+                done: true,
+            },
+            {
+                text: "Orecchini",
+                done: false,
+            },
+            {
+                text: "Sneakers",
+                done: true,
+            },
+        ],
+    },
+    methods: {
+        
+    },
+});
